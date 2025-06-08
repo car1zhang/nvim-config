c6195949bb0fcd3854f8a44d52e59dd3a7c8ec65
Carl Zhang <zhang.carl.x@gmail.com>
2025-06-08 15:49:02 -0400

    .

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,10 +6,11 @@ return {
         config = function()
             local builtin = require("telescope.builtin")
 
-            vim.keymap.set("n", "<leader><leader>", builtin.find_files, {})
-            vim.keymap.set('n', '<leader>P', builtin.oldfiles, {})
-            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+            vim.keymap.set('n', '<leader><leader>', builtin.oldfiles, {})
+            vim.keymap.set("n", "<leader>f", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+            vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
         end,
     }
 }
